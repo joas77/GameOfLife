45565699d819337c2ccc712524f4730269cf430e
Julio Angulo <solojoas@gmail.com>
2023-12-29 19:35:02 +0100

    Added toml config file

--- rplife/patterns.py
+++ rplife/patterns.py
@@ -5,3 +5,10 @@ from dataclasses import dataclass
 class Pattern:
     name: str
     alive_cells: Set[Tuple[int, int]]
+
+    @classmethod
+    def from_toml(cls, name, toml_data):
+        return cls(
+            name,
+            alive_cells = {tuple(cell) for cell in toml_data["alive_cells"]},
+        )
